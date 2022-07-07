2fbaf016816bad09baa74edf5e550495928d50e1
Victor Tesoura Júnior <txsoura@yahoo.com>
2022-07-07 20:40:01 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,21 +13,27 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "instance" {
   ami           = var.instance_ami
   instance_type = var.instance_type
   
   vpc_security_group_ids = [
-    aws_security_group.vpc.id
+    aws_security_group.default.id
   ]
 }
 
-resource "aws_security_group" "vpc" {
+resource "aws_vpc" "mainvpc" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.mainvpc.id
+
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
   }
 
   egress {
