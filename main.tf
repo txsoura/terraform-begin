6da8c432483fcb3a6ec6deca8c0b18a8b255d26b
Victor Tesoura Júnior <txsoura@yahoo.com>
2022-07-07 20:07:41 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.17.1"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "web" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update -y 
+    sudo apt install curl -y
+    sudo apt install git -y
+    sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+  EOF
+
+  vpc_security_group_ids = []
+}
